56a1a0b02c589219fb0c56bc4f930bfe16454deb
Inrikys <henrique661@gmail.com>
2020-10-24 14:09:44 -0300

    jwt installed

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/login', function (Request $request) {
+    return 'login';
+})->name('login');   
+
+Route::get('/teste', 'api\UserController@index')->name('teste.index');
+
+Route::group(
+    [
+        'middleware' => [''],
+        'namespace' => 'api'
+    ], function () {
+
+        Route::get('/users', 'UserController@index')->name('user.index'); 
+        Route::get('/users/{id}', 'UserController@show')->name('user.show'); 
+        Route::post('/users', 'UserController@store')->name('user.store');
+        Route::put('/users/{id}', 'UserController@update')->name('user.update'); 
+        Route::delete('/users/{id}', 'UserController@destroy')->name('user.destroy');  
 });
